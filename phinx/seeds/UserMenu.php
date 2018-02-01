d39907b36603008c50ed0745dafe7e588a19f743
Jihun Kim <devgrapher@ridi.com>
2018-02-01 15:58:27 +0900

    Add all menu paths for admin user

---
+++ phinx/seeds/UserMenu.php
@@ -0,0 +1,28 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class UserMenu extends AbstractSeed
+{
+    public function run()
+    {
+        $menus = $this->fetchAll('
+            select *
+            from tb_admin2_menu
+        ');
+
+        $user_menus = $this->table('tb_admin2_user_menu');
+        array_map(function($menu) use($user_menus) {
+            $data = [
+                [
+                    'user_id' => 'admin',
+                    'menu_id' => $menu['id'],
+                    'reg_date' => date('Y-m-d H:i:s'),
+                ],
+            ];
+            $user_menus->insert($data);
+
+        }, $menus);
+        $user_menus->save();
+    }
+}
